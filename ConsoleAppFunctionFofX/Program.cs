cdf222532b43c09c522816e338d2cfe73ff9bfc7
fred <domaine75@gmail.com>
2023-12-17 17:46:11 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleAppFunctionFofX/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleAppFunctionFofX
+{
+  internal class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("calcul");
+      for (int i = 0; i < 55; i++)
+      {
+        var test = Computefunction(i);
+        Console.WriteLine($"F de {i} est {test}");
+      }
+
+      Console.WriteLine("Press any key to exit:");
+      Console.ReadKey();
+    }
+
+    private static int Computefunction(int number)
+    {
+      return (((number * 2) + 6) / 2) - number;
+    }
+  }
+}
